be7ba933ac9378ecc9d8a68aad51d2fe3d1ac449
isso0424 <isso04240129@gmail.com>
2020-10-18 03:26:14 +0900

    [Add]Footer nanmo wakaran

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Main from "./src/main";
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +10,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  main: {
+    backgroundColor: "#aaa"
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}><ScrollView><Main/><Text>Aho</Text></ScrollView></View>
+      
+      <View style={styles.footer}><Text>Footer</Text></View>
+    </View>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  text: {
+    fontSize: 80
+  }
+});
+
+const Main = () => 
+  <View><Text style={style.text}>Hello, SHINE</Text></View>
+
+export default Main;
